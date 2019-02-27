7d864984e85d11a33b18c7a8684516ceaf361ce7
RICHARD Emmanuel - CAP-DIGITAL <emmanuel.richard@cap-digital.fr>
2019-02-27 18:00:00 +0100

    Update CapdigitalNTLMSoapClient.php

--- src/CapdigitalNTLMSoapClient.php
+++ src/CapdigitalNTLMSoapClient.php
@@ -3,7 +3,7 @@ namespace Capdigital\CapdigitalNTLMSoapClient;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class CapdigitalPlateformeBundle extends Bundle
+class CapdigitalNTLMSoapClient extends Bundle
 {
   public function say($toSay = "Nothing given")
     {
