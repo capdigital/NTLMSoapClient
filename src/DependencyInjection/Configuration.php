3bdb7ef1c3ef70913efc9eb67eec0f7d0559780b
RICHARD Emmanuel - CAP-DIGITAL <emmanuel.richard@cap-digital.fr>
2019-02-28 11:35:03 +0100

    Create Configuration.php

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Vich\UploaderBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\HttpKernel\Kernel;
+
+/**
+ * Configuration.
+ *
+ */
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        if (Kernel::VERSION_ID >= 40200) {
+            $builder = new TreeBuilder('capdigital_ntlm_soap_client');
+        } else {
+            $builder = new TreeBuilder();
+        }
+        $root = $builder->root('capdigital_ntlm_soap_client');
+        $this->addGeneralSection($root);
+
+        return $builder;
+    }
+
+    protected function addGeneralSection(ArrayNodeDefinition $node): void
+    {
+        $node
+            ->children()
+                ->scalarNode('user')
+                    ->defaultValue('_name')
+                ->end()
+            ->end()
+        ;
+    }
+
+   
+    
+}
