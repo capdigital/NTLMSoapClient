4b9d0850f784db7bf2dadca0d2126455892ad38c
RICHARD Emmanuel - CAP-DIGITAL <emmanuel.richard@cap-digital.fr>
2019-02-28 11:22:04 +0100

    Update CapdigitalNTLMSoapClient.php

--- src/CapdigitalNTLMSoapClient.php
+++ src/CapdigitalNTLMSoapClient.php
@@ -5,8 +5,43 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class CapdigitalNTLMSoapClient extends Bundle
 {
-  public function say($toSay = "Nothing given")
+    /**
+     * CapdigitalNTLMSoapClient constructor.
+     */
+    public function __construct($baseURL)
+    {
+        require_once("Class\NTLMStream.php");
+
+        require_once("Class\NTLMSoapClient.php");
+
+        stream_wrapper_unregister('http');
+
+        stream_wrapper_register('http', 'NTLMStream') or die("Failed to register protocol");
+
+        $client = new \NTLMSoapClient($baseURL);
+
+        // Find the first Company in the Companies
+        $result = $client->Companies();
+        $companies = $result->return_value;
+        echo "Companies:<br>";
+        if (is_array($companies)) {
+            foreach($companies as $company) {
+                echo "$company<br>";
+            }
+            $cur = $companies[0];
+        }
+        else {
+            echo "$companies<br>";
+            $cur = $companies;
+        }
+    }
+
+
+    public function say($toSay = "Nothing given")
     {
         return $toSay;
     }
+
+
+
 }
