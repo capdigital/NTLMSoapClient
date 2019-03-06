1d9664faa5dc3ce2e1f1b5ab1115677917f35115
RICHARD Emmanuel - CAP-DIGITAL <emmanuel.richard@cap-digital.fr>
2019-03-06 08:45:44 +0100

    Update Configuration.php

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -5,7 +5,7 @@ namespace Capdigital\NTLMSoapClient\DependencyInjection;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
-use Symfony\Component\HttpKernel\Kernel;
+
 
 /**
  * Configuration.
@@ -13,29 +13,25 @@ use Symfony\Component\HttpKernel\Kernel;
  */
 class Configuration implements ConfigurationInterface
 {
-    public function getConfigTreeBuilder(): TreeBuilder
-    {
-        if (Kernel::VERSION_ID >= 40200) {
-            $builder = new TreeBuilder('capdigital_ntlm_soap_client');
-        } else {
-            $builder = new TreeBuilder();
-        }
-        $root = $builder->root('capdigital_ntlm_soap_client');
-        $this->addGeneralSection($root);
 
-        return $builder;
-    }
-
-    protected function addGeneralSection(ArrayNodeDefinition $node): void
+    public function getConfigTreeBuilder()
     {
-        $node
+        $treeBuilder = new TreeBuilder();
+
+        $rootNode = $treeBuilder->root('capdigital_ntlm_soap_client');
+
+        $rootNode
             ->children()
-                ->scalarNode('user')
-                    ->defaultValue('_name')
-                ->end()
+            ->scalarNode('url')->end()
+            ->scalarNode('user')->end()
+            ->scalarNode('password')->end()
+            ->end()
             ->end()
         ;
+
+        return $treeBuilder;
     }
+
 
    
     
