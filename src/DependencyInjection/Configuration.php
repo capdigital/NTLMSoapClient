fedf4042d0ca2c49104f70e8f6cee5bcb7ae1289
RICHARD Emmanuel - CAP-DIGITAL <emmanuel.richard@cap-digital.fr>
2019-03-07 16:02:26 +0100

    Update Configuration.php

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Capdigital\NTLMSoapClient\DependencyInjection;
+namespace Capdigital\NtlmSoapClient\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
