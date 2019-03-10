a967187ee603fe0971d58ee35d15d3c0fc21cbe1
RICHARD Emmanuel - CAP-DIGITAL <emmanuel.richard@cap-digital.fr>
2019-03-10 11:25:25 +0100

    Add files via upload

--- src/CapdigitalNtlmSoapClient.php
+++ src/CapdigitalNtlmSoapClient.php
@@ -1,49 +1,17 @@
 <?php
 namespace Capdigital\NtlmSoapClient;
 
+use Interop\Container\ContainerInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Dump\Container;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
+use Capdigital\NtlmSoapClient\Service\MyService;
+
+
 
 class CapdigitalNtlmSoapClient extends Bundle
 {
-        
-
-    public function connect($baseURL)
-    {
-    require_once("Class\NTLMStream.php");
-
-    require_once("Class\NTLMSoapClient.php");
-
-    stream_wrapper_unregister('http');
-
-    stream_wrapper_register('http', 'NTLMStream') or die("Failed to register protocol");
-            
-            
-    $client = new \NtlmSoapClient($baseURL);
-            
-            
-
-        // Find the first Company in the Companies
-        $result = $client->Companies();
-        $companies = $result->return_value;
-        echo "Companies:<br>";
-        if (is_array($companies)) {
-            foreach($companies as $company) {
-                echo "$company<br>";
-            }
-            $cur = $companies[0];
-        }
-        else {
-            echo "$companies<br>";
-            $cur = $companies;
-        }
-    }
-
-
-    public function say($toSay = "Nothing given")
-    {
-        return $toSay;
-    }
-
-
 
 }
